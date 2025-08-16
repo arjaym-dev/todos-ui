ecd7e9299c58cc5df6349aa82d97cf92b7ce64ec
Arjay Marquez <arjay@techasiaportal.com>
2025-08-16 18:38:06 +0800

    test commit

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -14,12 +14,13 @@ export async function POST(request: Request) {
 
 		const payload: CreateUser = await request.json()
 
-		// Validate payload
+		// Validate payload Test
 		await createUserSchema.validate(payload, { abortEarly: false })
 
 		// Check assign roles & Validate if user allow for creating user account
 		const roles = await Role.find({})
 
+		console.log(roles)
 		// Hash password
 
 		// Save user into collection
