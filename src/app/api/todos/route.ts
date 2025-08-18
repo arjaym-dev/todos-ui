30275990e7f4a58aa8f3ce598b2e1d114779d1ed
arjaym-dev <arjaym.dev@gmail.com>
2025-08-18 13:32:16 +0800

    BE feat: Enable to delete task

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -11,6 +11,8 @@ import roles from "@/app/models/roles"
 import {
 	createTaskSchema,
 	deleteTaskSchema,
+	editTaskSchema,
+	TEditTask,
 	TCreateTask,
 	TDeleteTask,
 } from "@/shared/validation/todos"
@@ -82,6 +84,7 @@ export async function POST(req: Request) {
 	}
 }
 
+// Delete task
 export async function DELETE(req: Request) {
 	try {
 		await dbConnect()
@@ -128,6 +131,74 @@ export async function DELETE(req: Request) {
 		await todos.findByIdAndDelete(payload.taskId)
 
 		return Response.json({}, { status: 202 })
+	} catch (error) {
+		let resError: { [key: string]: string } = {},
+			status = 500
+
+		if (error instanceof ValidationError) {
+			// Validate payload send error message
+			resError = payloadValidationErrors(error)
+			status = 400
+		} else if (error instanceof MongooseError) {
+			resError = { message: error.message }
+			status = 400
+		}
+
+		return Response.json(resError, { status: status })
+	}
+}
+
+// Update task
+export async function PUT(req: Request) {
+	try {
+		await dbConnect()
+		const session = req.cookies.get("session")?.value
+		const sessionDecrypt = (await decrypt(session)) as JWTPayload
+
+		const payload: TEditTask = await req.json()
+
+		// Validate payload
+		await deleteTaskSchema.validate(payload, { abortEarly: false })
+
+		// Validate if the session token, is really the user that creating the task
+		const user = await users.findById(payload.userId)
+
+		if (user) {
+			if (user._id.toString() !== sessionDecrypt._id) {
+				return Response.json(
+					{
+						message:
+							"Session user does not match on user id, invalid action",
+					},
+					{ status: 401 },
+				)
+			}
+		}
+
+		// Validate if the user, has permission if not throw error
+		const role = await roles.findById(sessionDecrypt.roleId)
+		if (role) {
+			const permissions = role.permissions
+
+			if (permissions.includes("API_TODOS_DELETE") === false) {
+				return Response.json(
+					{
+						message:
+							"User does not have permission for this action",
+					},
+					{ status: 401 },
+				)
+			}
+		}
+
+		// Update the task
+		const task = await todos.findByIdAndUpdate(
+			payload.taskId,
+			{ task: payload.task },
+			{ lean: true, new: true, select: "_id userId task" },
+		)
+
+		return Response.json(task, { status: 200 })
 	} catch (error) {
 		let resError: { [key: string]: string } = {},
 			status = 500
--- src/shared/validation/todos.ts
+++ src/shared/validation/todos.ts
@@ -12,5 +12,12 @@ export const deleteTaskSchema = object({
 	taskId: string().required("TaskID is required"),
 })
 
+export const editTaskSchema = object({
+	userId: string().required("UserID is required"),
+	taskId: string().required("TaskID is required"),
+	task: string().required("Task is required"),
+})
+
 export type TCreateTask = InferType<typeof createTaskSchema>
 export type TDeleteTask = InferType<typeof deleteTaskSchema>
+export type TEditTask = InferType<typeof editTaskSchema>
