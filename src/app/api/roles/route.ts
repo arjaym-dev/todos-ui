5c5e72469b46fbbe243e7a4dda6979672dd6182b
arjaym-dev <arjaym.dev@gmail.com>
2025-08-26 22:28:12 +0800

    BE feat: Enable get roles api

---
+++ src/app/api/roles/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest as Request, NextResponse as Response } from "next/server"
+import { ValidationError } from "yup"
+import { MongooseError } from "mongoose"
+
+import dbConnect from "@/shared/lib/db-connect"
+import { payloadValidationErrors } from "@/shared/handler/payload-validation"
+
+import permissions from "@/app/models/permissions"
+import roles from "@/app/models/roles"
+
+export async function GET(req: Request) {
+	const roleId = req.nextUrl.searchParams.get("roleId")
+
+	try {
+		await dbConnect()
+
+		// Get roles
+		const rolesCollection = await roles.findById(roleId).lean(true)
+
+		// Get permissions
+		const permissionsCollection = await permissions.find({}, {}).lean(true)
+
+		return Response.json(
+			{ roles: rolesCollection, permissions: permissionsCollection },
+			{ status: 200 },
+		)
+	} catch (error) {
+		let resError: { [key: string]: string } = {},
+			status = 500
+
+		if (error instanceof ValidationError) {
+			// Validate payload send error message
+			resError = payloadValidationErrors(error)
+			status = 400
+		} else if (error instanceof MongooseError) {
+			resError = { message: error.message }
+			status = 400
+		}
+
+		return Response.json(resError, { status: status })
+	}
+}
