b0439a861696dd44d157b29b9004a7a3c26346bd
arjaym-dev <arjaym.dev@gmail.com>
2025-09-27 22:42:07 +0800

    UI feat: Added auth
    - Enable securing the route using client side
    	- Use of the HOC
    	- Apply it on the pages ff;
    		- dashboard
    		- todos
    		- roles

--- src/shared/lib/with-auth.tsx
+++ src/shared/lib/with-auth.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useRouter } from "next/navigation"
+import { useRouter, usePathname } from "next/navigation"
 
 import Box from "@mui/material/Box"
 import CircularProgress from "@mui/material/CircularProgress"
@@ -10,34 +10,42 @@ import { requestGetNavlinks } from "@/container/sidebar/request"
 const withAuth = (Content: React.ComponentType) => {
 	const Component = (props: any) => {
 		const router = useRouter()
+		const pathname = usePathname()
+
+		const [render, setRender] = React.useState(false)
+
 		const { user } = useTodoStore()
 
 		const { data: navlinks, isPending } = requestGetNavlinks(user.token)
 
-		let content = null
-
-		React.useEffect(() => {
-			if (isPending === false) {
+		React.useLayoutEffect(() => {
+			if (!isPending) {
 				const navSet = new Set(
 						navlinks.map(
 							(nav: { name: string; path: string }) => nav.path,
 						),
 					) as Set<string>,
-					hasNavlinks = navSet.has("/dashboard")
+					hasNavlinks = navSet.has(pathname)
 
-				console.log(hasNavlinks)
-				if (hasNavlinks === false) {
+				if (!hasNavlinks) {
 					router.push(navlinks[0].path)
-				}
-			}
-		}, [isPending, navlinks])
 
-		if (isPending)
+					return
+				}
+
+				setRender(true)
+			}
+		}, [isPending])
+
+		if (isPending) {
 			return (
 				<Box sx={{ display: "flex", justifyContent: "center" }}>
 					<CircularProgress />
 				</Box>
 			)
+		}
+
+		if (!render) return null
 
 		return <Content {...props} />
 	}
